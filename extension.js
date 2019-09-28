a022e281db41d86d512a2ef253396695c14453fc
Nate Fatkhiyev <nfatkhiyev@localhost.localdomain>
2019-09-28 15:47:00 -0400

    Trying to pass parameters to functions using the block. Just a test.

--- extension.js
+++ extension.js
@@ -2,6 +2,12 @@
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
+    ext.request(method, url){
+	   requestMethod = method.toUpperCase();
+	   fetch('url',
+          	{method: 'method', body: JSON.stringify(params)})
+          	.catch((error) => console.log(error));
+    }
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
@@ -11,6 +17,7 @@
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
+	    ['R', '%n from/to %s', 'request', 'GET', '255.255.255.255']
         ]
     };
 
